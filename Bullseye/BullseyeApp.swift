93cde5fdfcc99d61d632cb307b4cd2b6a270accb
Jiexin LI <ljxxx1206@gmail.com>
2022-05-26 16:21:20 +0930

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Lee on 26/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
